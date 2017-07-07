ca9569f29a183abc90189dc627aecaec7484c51c
dany <danimaxi60@hotmail.com>
2017-07-07 06:18:38 -0500

    archivo php

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        
+        $this->assertEquals(1+1,2);
+        
+
+      
+        $this->assertEquals('Home'($title));
+    }
+}
+?>
